4f1cd457455a74e8d906c80500521df25b7043ca
LashenDEV <lashenkari@gmail.com>
2022-10-01 12:06:12 +0530

    reverse an array

--- php/dasundev.php
+++ php/dasundev.php
@@ -2,10 +2,18 @@
 
 // Return the Sum of Two Numbers #1
 
-function sum($x, $y) {
-	return $x + $y;
+function sum($x, $y)
+{
+    return $x + $y;
 }
 
 echo "Sum of Two Numbers: " . sum(5, 6);
+
+function reverseAnArray($array)
+{
+    return print_r(array_reverse($array));
+}
+
+echo "Reverse an array : " . reverseAnArray([1, 2, 3, 4]);
 
 ?>
